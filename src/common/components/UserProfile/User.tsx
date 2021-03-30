432479d1bc598d251343c6a9446289a659d4bbbf
MaccabiDev <37658179+MaccabiDev@users.noreply.github.com>
2021-03-31 00:48:03 +0300

    as

--- src/common/components/UserProfile/User.tsx
+++ src/common/components/UserProfile/User.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { Dispatch } from "redux";
 import { useDispatch } from "react-redux";
 import Button from "@material-ui/core/Button";
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import { createStore, applyMiddleware, Store } from "redux";
 import { Provider } from "react-redux";
